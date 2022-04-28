522b6abdeb248c5e9179224ea951c965caa8911b
mbraha <michael.c.braha@gmail.com>
2022-04-28 13:25:21 -0700

    Update and add a bunch of roles

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# Install Ansible
-# sudo yum install -y ansible cowsay
+# Install Ansible, which will perform rest of setup.
+sudo yum install -y ansible cowsay
+
+# Install things Ansible needs to run
+sudo yum install -y python3-psutil
+ansible-galaxy collection install community.general
 
 # Run playbook
 ansible-playbook setup.yml
