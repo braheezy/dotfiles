b8a608162610eb6faea7afc3baf2a7467e1fdf3d
braheezy <michael.c.braha@gmail.com>
2023-01-17 11:30:47 -0800

    many updates

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ SCRIPT_DIR="`dirname $0`"
 
 if ! command -v gum &>/dev/null
 then
-    # Quick! Install Gum!
-    sudo echo '[charm]
-    name=Charm
-    baseurl=https://repo.charm.sh/yum/
-    enabled=1
-    gpgcheck=1
-    gpgkey=https://repo.charm.sh/yum/gpg.key' > /etc/yum.repos.d/charm.repo
-    sudo yum install gum 2>/dev/null
+    echo "Installing Gum. This may take a minute..."
+    sudo bash -c 'echo "[charm]
+name=Charm
+baseurl=https://repo.charm.sh/yum/
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.charm.sh/yum/gpg.key" > /etc/yum.repos.d/charm.repo'
+    sudo yum install -y gum &>/dev/null
 fi
 
 wait_command()
--- roles/wallpapers/files/update-backgrounds.sh
+++ roles/wallpapers/files/update-backgrounds.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Opinionated setup of wallpapers
 
+set -uo pipefail
+
 current_wallpapers="$HOME/Pictures/current-wallpapers"
 base_wallpapers="$HOME/Pictures/catppuccin-wallpapers"
 
@@ -11,7 +13,21 @@ command pushd $base_wallpapers >/dev/null
     gum spin "Updating wallpapers..." git pull
 command popd >/dev/null
 
+getImages() {
+    html_string=$(cat README.md)
+    urls=$(grep -ozE '<img\s+src="https[^"]*"[^>]*>' <<< "$html_string" | perl -nle 'print $1 while /(https:\/\/[^"]*)/g')
+    i=0
+    for url in $urls
+    do
+        gum spin --title "Getting extra wallpapers..." -- wget $url -O "$(basename $PWD)-$i"
+        i=$((i + 1))
+    done
+}
+
 for folder in flatppuccin gradients landscapes minimalistic misc waves
 do
+    command pushd $base_wallpapers/$folder >/dev/null
+        getImages
+    command popd >/dev/null
     rsync -a $base_wallpapers/$folder/ $current_wallpapers
 done
