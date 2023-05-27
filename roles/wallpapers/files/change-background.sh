d5d1c9988fb2e9a1e9f43052c4f6f92f8f1882d0
braheezy <michael.c.braha@gmail.com>
2023-05-27 15:27:17 -0700

    Fix file URI path

--- roles/wallpapers/files/change-background.sh
+++ roles/wallpapers/files/change-background.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 folder=$HOME/Pictures/wallpapers/src
 random_pic=$(ls $folder/*.{png,jpg} | shuf -n1)
 
-dconf write /org/gnome/desktop/background/picture-uri "'ansible.builtin.file://$random_pic'"
-dconf write /org/gnome/desktop/background/picture-uri-dark "'ansible.builtin.file://$random_pic'"
+dconf write /org/gnome/desktop/background/picture-uri "'file://$random_pic'"
+dconf write /org/gnome/desktop/background/picture-uri-dark "'file://$random_pic'"
 
 # Pic a different pic for the screensaver
-dconf write /org/gnome/desktop/screensaver/picture-uri "'ansible.builtin.file://$(ls $folder/*.png | shuf -n1)'"
+dconf write /org/gnome/desktop/screensaver/picture-uri "'file://$(ls $folder/*.png | shuf -n1)'"
