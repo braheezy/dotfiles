e3997f2631ffcc99becc62157cdb8fa051f483d2
braheezy <michael.c.braha@gmail.com>
2023-06-10 10:46:33 -0700

    Add install script args

--- install.sh
+++ install.sh
@@ -2,28 +2,53 @@
 
 set -euo pipefail
 
-SCRIPT_DIR="`dirname $0`"
+DO_INSTALL=true
+DRY_RUN=false
 
-if ! command -v gum &>/dev/null
-then
-    echo "Installing Gum. This may take a minute..."
-    sudo bash -c 'echo "[charm]
-name=Charm
-baseurl=https://repo.charm.sh/yum/
-enabled=1
-gpgcheck=1
-gpgkey=https://repo.charm.sh/yum/gpg.key" > /etc/yum.repos.d/charm.repo'
-    sudo yum install -y gum &>/dev/null
+# Process command line options
+while getopts ":dn" opt; do
+  case $opt in
+    n)
+      DO_INSTALL=false
+      ;;
+    d)
+      DRY_RUN=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+SCRIPT_DIR=$(dirname "$0")
+
+GUM_INSTALLED=false
+if ! command -v gum &>/dev/null; then
+    if $DO_INSTALL; then
+        echo "$(tput setaf 3)Installing Gum. This may take a minute...$(tput sgr 0)"
+        wget -qO- \
+            https://github.com/charmbracelet/gum/releases/download/v0.10.0/gum_0.10.0_Linux_x86_64.tar.gz \
+            | tar -xz gum
+        sudo mv gum /usr/local/bin
+        GUM_INSTALLED=true
+    fi
+else
+    GUM_INSTALLED=true
 fi
 
-if [ -z ${CI+z} ]; then
+if [ -z ${CI+z} && $GUM_INSTALLED ]; then
+    # Run commands behind a pretty spinner
     print_and_run() {
         title=$1
         shift
         cmd=$@
         gum spin --spinner minidot --title "$title" -- $cmd
     }
+    export SPOTIFY_CLIENT_ID=$(skate get spotify_client_id)
 else
+    # Simply print and run commands
     print_and_run() {
         echo "$1" && shift && "$@"
     }
@@ -38,9 +63,13 @@ RUN_PREFIX="print_and_run"
 
 export ANSIBLE_CONFIG="$SCRIPT_DIR/ansible.cfg"
 export ANSIBLE_COW_SELECTION=hellokitty
-if [ -z ${DRY+z} ]; then
-    ansible-playbook "$SCRIPT_DIR/setup.yml"
-else
-    gum style --foreground $YELLOW --bold --italic "Running setup in dry-run mode..."
-    ansible-playbook "$SCRIPT_DIR/setup.yml" --check --diff
+
+ANSIBLE_ARGS=""
+if ! $DO_INSTALL; then
+    ANSIBLE_ARGS+=" -e do_install=false"
 fi
+if $DRY_RUN; then
+    ANSIBLE_ARGS+=" --check --diff"
+fi
+
+ansible-playbook "$SCRIPT_DIR/setup.yml" $ANSIBLE_ARGS
